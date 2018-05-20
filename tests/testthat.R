671bd0e037a4db1d9031ea62b1a7f317aacbc7de
cheng-p <cheng.vince.peng@gmail.com>
2018-05-20 00:34:04 -0700

    Update testthat and relevant files.

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
 library(testthat)
-library(LCid)
+library(LUCid)
 
-test_check("LCid")
-=======
-library(testthat)
-library(LCid)
-
-test_check("LCid")
->>>>>>> cc8b3b638db6d8718bc23a1d71e9064c33b4c1ad
+test_check("LUCid")
