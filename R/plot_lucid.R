c6e4b553cb83e7f3a36019500c954e0157b7ad60
cheng-p <cheng.vince.peng@gmail.com>
2018-09-27 19:23:56 -0700

    Update the plot_lucid function for clustering with a binary outcome of interest.

--- R/plot_lucid.R
+++ R/plot_lucid.R
@@ -2,6 +2,7 @@
 #'
 #'\code{plot_lucid} generates a Sankey diagram for the results of integrative clustering based on an \code{IntClust} object.
 #' @param x An \code{IntClust} class object
+#' @param switch An indicator to do label switching with a descending order in gamma or not, the default is FALSE
 #' @param colorScale D3 color scheme for the Sankey diagram
 #' @export
 #' @import networkD3
@@ -9,9 +10,12 @@
 #' @references
 #' Peng, C., Yang, Z., Conti, D.V.
 
-plot_lucid <- function(x, colorScale=default) {
+plot_lucid <- function(x, switch = FALSE, colorScale=default) {
 
-  default <- 'd3.scaleOrdinal() .domain(["0", "1", "2", "3", "4", "5"]) .range(["lightsteelblue", "royalblue", "mediumorchid", "gold", "cyan", "tan"])'
+  default <- 'd3.scaleOrdinal() .domain(["0", "1", "2", "3", "4", "5", "6", "7", "8"]) .range(["lightsteelblue", "royalblue", "mediumorchid", "gold", "cyan", "tan", "darkgray", "red", "green"])'
+
+  family <- x$family
+  K <- x$K
 
   name <- c(x$Gnames, paste0("IntClust", 1:x$K), x$Znames, "Outcome")
   nodegroup <- c(rep(2, length(x$Gnames)), rep(3, x$K), rep(4, length(x$Znames)), 5)
@@ -19,11 +23,24 @@ plot_lucid <- function(x, colorScale=default) {
 
   source <- c(rep(0:(length(x$Gnames)-1), x$K), rep(length(x$Gnames):(length(x$Gnames)+x$K-1), each=length(x$Znames)), length(x$Gnames):(length(x$Gnames)+x$K-1))
   target <- c(rep(length(x$Gnames):(length(x$Gnames)+x$K-1), each=length(x$Gnames)), rep((length(x$Gnames)+x$K):(length(x$Gnames)+x$K+length(x$Znames)-1), x$K), rep(length(x$Gnames)+x$K+length(x$Znames), x$K))
-  orgvalue <- c(c(t(x$beta[,-1])), c(t(x$mu)), x$gamma[1:x$K])
+  orgvalue <- c(c(t(summary_lucid(x, switch = switch)$Beta[,-1])), c(t(summary_lucid(x, switch = switch)$Mu)), summary_lucid(x, switch = switch)[[3]])
   linkgroup <- numeric()
   value <- numeric()
   for (i in 1:length(orgvalue)) {
     linkgroup[i] <- ifelse(orgvalue[i]<0, 0, 1)
+  }
+  if(family == "binary") {
+    for (i in (length(orgvalue)-K+1):length(orgvalue)) {
+      if (orgvalue[i]==1) {
+        linkgroup[i]=6
+      }
+      if (orgvalue[i]>1) {
+        linkgroup[i]=7
+      }
+      if (orgvalue[i]<1) {
+        linkgroup[i]=8
+      }
+    }
   }
   for (i in 1:length(orgvalue)) {
     value[i] <- ifelse(orgvalue[i]<0, abs(orgvalue[i]), orgvalue[i])
